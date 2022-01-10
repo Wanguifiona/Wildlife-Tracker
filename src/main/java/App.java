8ce4241a106ae4f0504a92a23f0f9b459837163d
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-10 14:20:47 +0300

    add animal-form template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,15 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "about.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/animals-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animals-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/animal-sighting", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "animal-sighting.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
