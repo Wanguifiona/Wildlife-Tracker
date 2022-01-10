43245580b539fc7a09fb527642a66cd47c2de61f
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-10 17:00:13 +0300

    created database classes

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,17 @@
 package models;
 
-public class Animal {
+public class Animal  {
+    private String name;
+    private int id;
+
+    public Animal(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
 }
--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,7 +1,18 @@
 package models;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void person_instantiatesCorrectly_true() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        assertEquals(true, testPerson instanceof Person);
+    }
 
 }
