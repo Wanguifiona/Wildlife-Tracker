34a192b32d41b6fd3ecab2e07aeb2c013ce8b077
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-12 20:47:16 +0300

    final commit

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -3,8 +3,9 @@ package models;
 import org.sql2o.*;
 
 public class DB {
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-227-15-75.compute-1.amazonaws.com:5432/d7kt9nr4lpvou9", "brphmeewosowzv", "c44a05b260a3b5cd50bb630349ff7ccab172071c590af306441854372145f8df");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-227-15-75.compute-1.amazonaws.com:5432/d7kt9nr4lpvou9", "brphmeewosowzv", "c44a05b260a3b5cd50bb630349ff7ccab172071c590af306441854372145f8df");
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "fiona", "Man");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "fiona", "Man");
+
 }
 
