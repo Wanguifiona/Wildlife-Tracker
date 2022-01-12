66b367e68486267a166a7cece2abb8ce5ea76137
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-12 20:27:51 +0300

    final commit

--- src/test/java/models/SightingsTest.java
+++ src/test/java/models/SightingsTest.java
@@ -11,42 +11,42 @@ public class SightingsTest {
 
     @Test
     public void sighting_instantiatesCorrectly_true() {
-        Sightings testSighting = new Sightings("Mike", 1,"Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
         assertEquals(true, testSighting instanceof Sightings);
     }
 
     @Test
     public void getRangerName_sightingInstantiatesWithrangerName_true() {
-        Sightings testSighting = new Sightings("Mike", 1,"Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
         assertEquals("Mike", testSighting.getRangerName());
     }
     @Test
     public void getAnimalId_sightingInstantiatesWithanimalId_true() {
-        Sightings testSighting = new Sightings("Mike", 1,"Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
         assertEquals(1, testSighting.getAnimalId());
     }
     @Test
     public void getLocation_sightingInstantiatesWithlocation_true() {
-        Sightings testSighting = new Sightings("Mike", 1,"Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
         assertEquals("Zone A", testSighting.getLocation());
     }
     @Test
     public void equals_returnsTrueIfRangerNameAreSame_true() {
-        Sightings testSighting = new Sightings("Mike", 1,"Zone A");
-        Sightings anotherSighting = new Sightings("Mike", 1,"Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
+        Sightings anotherSighting = new Sightings(1, "Mike","Zone A");
         assertTrue(testSighting.equals(anotherSighting));
     }
     @Test
     public void save_successfullyAddsSightingToDatabase_List() {
-        Sightings testSighting = new Sightings("Mike", 1,"Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
         testSighting.save();
         assertTrue(Sightings.all().get(0).equals(testSighting));
     }
     @Test
     public void all_returnsAllInstancesOfAnimal_true() {
-        Sightings firstSighting = new Sightings("Mike", 1, "Zone A");
+        Sightings firstSighting = new Sightings(1, "Mike", "Zone A");
         firstSighting.save();
-        Sightings secondSighting = new Sightings("Paul", 2,"Near the river");
+        Sightings secondSighting = new Sightings(2, "Paul","Near the river");
         secondSighting.save();
         assertEquals(true, Sightings.all().get(0).equals(firstSighting));
         assertEquals(true, Sightings.all().get(1).equals(secondSighting));
@@ -54,14 +54,14 @@ public class SightingsTest {
     }
     @Test
     public void save_assignsIdToSighting() {
-        Sightings testSighting = new Sightings("Mike",1, "Zone A");
+        Sightings testSighting = new Sightings(1, "Mike", "Zone A");
         testSighting.save();
         Sightings savedSighting = Sightings.all().get(0);
         assertEquals(savedSighting.getId(), testSighting.getId());
     }
     @Test
     public void find_returnsSightingWithSameId_secondSighting() {
-        Sightings firstSighting = new Sightings("Mike", 1, "Zone A");
+        Sightings firstSighting = new Sightings(1, "Mike", "Zone A");
         firstSighting.save();
         Sightings secondSighting =  Sightings.all().get(0);
         secondSighting.save();
