3bb5a0e396a3fc1fa3f89280d0b1487ccbb0dc9d
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2022-01-13 11:07:59 +0300

    change database rule link

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -8,8 +8,8 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-//        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-3-227-15-75.compute-1.amazonaws.com:5432/d7kt9nr4lpvou9", "brphmeewosowzv", "c44a05b260a3b5cd50bb630349ff7ccab172071c590af306441854372145f8df");
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "fiona", "Man");
+        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-3-227-15-75.compute-1.amazonaws.com:5432/d7kt9nr4lpvou9", "brphmeewosowzv", "c44a05b260a3b5cd50bb630349ff7ccab172071c590af306441854372145f8df");
+//        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "fiona", "Man");
     }
 
     @Override
